94135b6922fdb06fe41be28d482a6b9150825d85
jonybang <jonybang@mail.ru>
2015-07-02 21:23:59 +1000

    пофиксил ошибку создания пользователя

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < InheritsController
 
   def create
     @user = User.new(user_params)
-    @user.contact = Contact.new({email:@user.email})
+    @user.contact = Contact.new({name:@user.email, contact_data: {email:@user.email}})
     if @user.save
-      redirect_to root_url, :notice => 'Signed up!'
+      redirect_to log_in_url, :notice => 'Signed up!'
     else
       render 'new'
     end

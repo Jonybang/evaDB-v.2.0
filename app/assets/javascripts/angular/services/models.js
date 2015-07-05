3673686b226eaadf6b01506403b3ec292dcec141
jonybang <jonybang@mail.ru>
2015-07-06 00:28:30 +1000

    Теперь работает добавление, редактирование задач вместе с датами(аллилуя)

--- app/assets/javascripts/angular/services/models.js
+++ app/assets/javascripts/angular/services/models.js
@@ -1,10 +1,24 @@
 'use strict';
 
+function parseResponseDates(response) {
+    var data = response.resource, key, value;
+    for (key in data) {
+        if(Object.prototype.toString.call(data[key]) === '[object Date]')
+            data[key] = data[key].getFullYear() +'-'+ (data[key].getMonth()+1) +'-'+ data[key].getDate();
+    }
+    return response;
+}
+
 var app = angular.module('app');
 app.factory('Project', ['$resource', function($resource) {
     return $resource('/api/projects/:id', {id: '@id'}, {'update': {method: 'PUT'}});
 }]);
 
 app.factory('ProjectTask', ['$resource', function($resource) {
-    return $resource('/api/projects/:project_id/project_tasks/:id', {project_id: '@project_id', id: '@id'}, {'update': {method: 'PUT'}});
+    return $resource('/api/projects/:project_id/project_tasks/:id',
+        {project_id: '@project_id', id: '@id'},
+        {'update': {
+            method: 'PUT',
+            interceptor: {response: parseResponseDates}
+        }});
 }]);

fee638ec8bb2221ec69c993693feef11664a2105
jonybang <jonybang@mail.ru>
2015-07-10 05:42:10 +1000

    вывод последних добавленных задач на странице

--- app/assets/javascripts/angular/controllers/main-controller.js
+++ app/assets/javascripts/angular/controllers/main-controller.js
@@ -1,7 +1,7 @@
 /**
  * Created by jonybang on 10.07.15.
  */
-angular.module('app').controller('MainCtrl', ['ProjectTask',
-    function(ProjectTask) {
-
+angular.module('app').controller('MainCtrl', ['$scope', 'ProjectTask',
+    function($scope, ProjectTask) {
+        $scope.last_tasks = ProjectTask.query({sort: 'created_at DESC', limit: 5});
     }]);

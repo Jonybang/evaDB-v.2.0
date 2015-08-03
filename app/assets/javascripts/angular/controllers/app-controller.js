d3c319a107b26c7857b87f6ba7ff99e05b673311
jonybang <jonybang@mail.ru>
2015-08-04 02:22:53 +1000

    вывод на аватарке имя текущего пользователя

--- app/assets/javascripts/angular/controllers/app-controller.js
+++ app/assets/javascripts/angular/controllers/app-controller.js
@@ -1,8 +1,12 @@
 /**
  * Created by jonybang on 10.07.15.
  */
-angular.module('app').controller('AppCtrl', ['$scope', '$location', 'ProjectTask',
-    function($scope, $location, ProjectTask) {
+angular.module('app').controller('AppCtrl', ['$scope', '$location', 'ProjectTask', 'User',
+    function($scope, $location, ProjectTask, User) {
+        User.get_contact().then(function(result){
+            $scope.contact = result;
+        });
+
         ProjectTask.query({sort: 'created_at DESC', limit: 3}).then(function (tasks) {
             $scope.last_tasks = tasks;
         });

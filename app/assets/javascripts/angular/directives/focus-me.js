b26b07fd9d1979ab52bdef9976d775432aacf88c
jonybang <jonybang@mail.ru>
2015-08-04 21:57:38 +1000

    сделал фокус на имени задачи про открытии окна

---
+++ app/assets/javascripts/angular/directives/focus-me.js
@@ -0,0 +1,15 @@
+angular.module('app').directive('focusMe', ['$timeout', function($timeout) {
+    return {
+        link: function(scope, element, attrs) {
+            scope.$watch(attrs.focusMe, function(value) {
+                if(value === true) {
+                    console.log('value=',value);
+                    $timeout(function() {
+                        element[0].focus();
+                        scope[attrs.focusMe] = false;
+                    });
+                }
+            });
+        }
+    };
+}]);
